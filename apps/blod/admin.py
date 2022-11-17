ae2364d7821c5b04cf20d385954ed352209be8c9
Markiz888 <markiz888n@gmail.com>
2022-11-18 01:53:29 +0200

    gg

--- apps/blod/admin.py
+++ apps/blod/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from apps.blod.models import BlogCategory
 from apps.blod.models import Articl
+from apps.blod.models import Tag
 
 
 
 
 admin.site.register(BlogCategory)
+admin.site.register(Tag)
 admin.site.register(Articl)
