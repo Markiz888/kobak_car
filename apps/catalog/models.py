0b97b5ded89f890960b50ff0bd66ec8c9a49fe4c
Markiz888 <markiz888n@gmail.com>
2022-12-28 16:52:52 +0200

    homework 30 12

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -51,3 +51,16 @@ class Category(MPTTModel):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Товар", max_length=255)
+    slug = models.SlugField(unique=True, verbose_name='Слаг (ЧПУ)')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    quanity = models.IntegerField(default=1)
+    price = models.DecimalField(verbose_name='Цена', max_digits=12, decimal_places=2, default=0)
+    updated_at = models.DateTimeField(verbose_name="Дата изменения", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
