92701a5940a6132b3ed0951610d904716cc87917
Markiz888 <markiz888n@gmail.com>
2023-01-24 18:09:12 +0200

    order

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.order.views import add_to_cart_view, cart_view
+from apps.order.views import add_to_cart_view, cart_view, create_order_view
 
 
 urlpatterns = [
     path('cart/', cart_view, name='cart'),
     path('add-to-cart/', add_to_cart_view, name='add-to-cart'),
+    path('create/', create_order_view, name='create_order'),
 ]
