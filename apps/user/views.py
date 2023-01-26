89f5a4b9577b425b8cc35e9a132b92888e96e340
Markiz888 <markiz888n@gmail.com>
2023-01-26 13:37:03 +0200

    homework 27 01

--- apps/user/views.py
+++ apps/user/views.py
@@ -6,7 +6,7 @@ from apps.user.forms import LoginForm, RegisterForm
 
 def user_login(request):
     error = None
-    breadcrumbs = {'current': 'Регистрация пользователя'}
+    breadcrumbs = {'current': 'Вход в аккаунт'}
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -19,6 +19,7 @@ def user_login(request):
     return render(request, 'user/login.html', {'error': error, 'breadcrumbs': breadcrumbs})
 
 def user_register(request):
+    breadcrumbs = {'current': 'Регистрация пользователя'}
     error = None
     if request.method == "POST":
         form = RegisterForm(request.POST, request.FILES)
@@ -26,9 +27,10 @@ def user_register(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return render(request, 'user/welcome.html', {'user': user})
+            breadcrumbs = {'current': 'Пользователь создан'}
+            return render(request, 'user/welcome.html', {'user': user, 'breadcrumbs': breadcrumbs})
         error = form.errors
-    return render(request, 'user/register.html', {'error': error})
+    return render(request, 'user/register.html', {'error': error, 'breadcrumbs': breadcrumbs})
 
 
 def user_logout(request):
