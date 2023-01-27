ddede5f4fbc8dcfe5426b9a7fc6726a3e8fe0463
Markiz888 <markiz888n@gmail.com>
2023-01-27 17:56:19 +0200

    order products

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.order.views import add_to_cart_view, cart_view, create_order_view
+from apps.order.views import add_to_cart_view, cart_view, create_order_view, delete_form_cart_view
 
 
 urlpatterns = [
     path('cart/', cart_view, name='cart'),
     path('add-to-cart/', add_to_cart_view, name='add-to-cart'),
     path('create/', create_order_view, name='create_order'),
+    path('delete/<int:product_id>/', delete_form_cart_view, name='delete_form_cart'),
 ]
