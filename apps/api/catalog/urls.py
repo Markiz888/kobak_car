46315a7c512407f7b9fd53b1c1123e39277ef83c
Markiz888 <markiz888n@gmail.com>
2023-02-13 22:49:23 +0200

    homework 14 02

--- apps/api/catalog/urls.py
+++ apps/api/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.api.catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView
+    ProductDeleteView, CategoryView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view()),
     path('product/update/<int:pk>/', ProductUpdateView.as_view()),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view()),
+    path('category/', CategoryView.as_view())
 ]
