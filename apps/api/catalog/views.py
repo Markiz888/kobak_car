5a104a7d53a7739f7947e86c326b655bda84cc25
Markiz888 <markiz888n@gmail.com>
2023-02-14 18:06:20 +0200

    drf part 3

--- apps/api/catalog/views.py
+++ apps/api/catalog/views.py
@@ -1,11 +1,26 @@
 from rest_framework import generics, permissions, viewsets
 from apps.catalog.models import Category, Product, ProductImage
-from apps.api.catalog.serializers import CategorySerializer, ProductReadSerializer, ProductWriteSerializer
+from apps.api.catalog.serializers import CategorySerializer, ProductReadSerializer, ProductWriteSerializer, \
+    ProductImageSerializer
 
 
 class ProductListView(generics.ListAPIView):
     serializer_class = ProductReadSerializer
-    queryset = Product.objects.all()
+
+    # queryset = Product.objects.filter(is_checked=True)
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(is_checked=True)
+
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
 
 
 class ProductDetailView(generics.RetrieveAPIView):
@@ -18,11 +33,14 @@ class ProductCreateView(generics.CreateAPIView):
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAdminUser]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class ProductUpdateView(generics.UpdateAPIView):
     serializer_class = ProductWriteSerializer
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAdminUser]
-
 
 
 class ProductDeleteView(generics.DestroyAPIView):
@@ -30,12 +48,21 @@ class ProductDeleteView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAdminUser]
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
     def get_permissions(self):
-        if self.action in ['create', 'update', 'update', 'destroy']:
+        if self.action in ['create', 'update' 'destroy']:
             return [permission() for permission in [permissions.IsAdminUser]]
         return [permission() for permission in [permissions.AllowAny]]
 
+class ProductImageViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create', 'update' 'destroy']:
+            return [permission() for permission in [permissions.IsAdminUser]]
+        return [permission() for permission in [permissions.AllowAny]]
