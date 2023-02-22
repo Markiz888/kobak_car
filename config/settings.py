9424230ba3f59d4a4a76723b34631ea5bcd16349
Markiz888 <markiz888n@gmail.com>
2023-02-22 21:29:54 +0200

    info pages

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'mathfilters',
     'rest_framework',
     'rest_framework.authtoken',
+    'tinymce',
 
     'apps.blog',
     'apps.user',
--- config/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def index(requst):
-    return render(requst, 'index.html')
