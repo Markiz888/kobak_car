0c740f2e91599ef70fef1426ec4e1636e14d8e0b
Markiz888 <markiz888n@gmail.com>
2023-02-28 19:49:37 +0200

    comments_2.2

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.blog.views import blog_category_list, article_list, article_view, tag_view
+from apps.blog.views import blog_category_list, article_list, article_view, tag_view, create_comment
 
 urlpatterns = [
     path('', blog_category_list, name='blog_category_list'),
     path('<int:category_id>/', article_list, name='blog_article_list'),
     path('tag/<int:tag_id>/', tag_view, name='blog_tag_view'),
     path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view'),
+    path('comment/<int:article_id>/', create_comment, name='create_comment'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.shortcuts import render
 
 from apps.blog.models import BlogCategory, Article, Tag, Comment
-from apps.blog.forms import CreateCommentForm
+from apps.blog.forms import CommentForm
 
 
 # Create your views here.
@@ -26,31 +26,18 @@ def article_list(request, category_id):
 def article_view(request, category_id, article_id):
     category = BlogCategory.objects.get(id=category_id)
     article = Article.objects.get(id=article_id)
-    comments = Comment.objects.filter(article=article).filter(is_checked=True)
+    comments = Comment.objects.filter(article=article, is_checked=True)
     breadcrumbs = {
         reverse('blog_category_list'): 'Блог',
         reverse('blog_article_list', args=[category_id]): category.name,
         'current': article.title
     }
-    error = None
-
-    if request.method == 'POST':
-        data = request.POST.copy()
-        data.update(article=article)
-        user = request.user
-        if not user.is_anonymous:
-            data.update(name=user.username, email=user.email, is_checked=True)
-        request.POST = data
-        form = CreateCommentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            breadcrumbs = {'current': 'Комментарий создан'}
-            return render(request, 'blog/comment_created.html', {'article': article, 'breadcrumbs': breadcrumbs})
-        else:
-            error = form.errors
     return render(request,
-                  'blog/article_view.html', {"category": category, "article": article, 'breadcrumbs': breadcrumbs,
-                                             'error': error, 'comments': comments})
+                  'blog/article_view.html', {
+                      "category": category,
+                      "article": article,
+                      'breadcrumbs': breadcrumbs,
+                      'comments': comments})
 
 
 def tag_view(request, tag_id):
@@ -62,3 +49,25 @@ def tag_view(request, tag_id):
     }
     return render(request, 'blog/tag_view.html', {'tag': tag, 'articles': articles, 'breadcrumbs': breadcrumbs})
 
+def create_comment(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if request.method == 'POST':
+        data = request.POST.copy()
+        data.update(article=article_id)
+
+        if not request.user.is_anonymous:
+            user = request.user
+            data.update({"name": f"{user.last_name} {user.first_name}",
+                         "is_checked": True,
+                         "email": user.email,
+                         "user": user})
+
+        request.POST = data
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save()
+            breadcrumbs = {'current': 'Комментарий успешно добавлен'}
+            return render(request, 'blog/comment_created.html', {
+                "comment": comment,
+                "breadcrumbs": breadcrumbs,
+                "article": article})
